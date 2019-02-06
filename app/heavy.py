0657accae6fdd2e1d00941e076f2ad4cc38dd587
Kevin Carmona-Murphy <kevcom@live.ca>
2019-02-06 17:20:47 -0500

    some solid refactoring and addition of comments to heavy.py

--- app/heavy.py
+++ app/heavy.py
@@ -1,9 +1,4 @@
-import os
-import magic
-import json
-from datetime import datetime
-import pytz
-from django.utils import timezone
+from os import scandir
 
 from app.metadata import MetaData
 from app.path import GalleryPath
@@ -13,13 +8,11 @@ from django.conf import settings
 from app.thumbnails import generate_thumbnails_if_missing
 
 import logging
-from sys import stdout
 
 from pathlib import PurePath
 
 from app.graphmethods import GraphMethods
 from app.fileutils import FileUtils
-
 
 def heavy_get(path):
   """
@@ -47,45 +40,112 @@ def heavy_get(path):
 
     # check if path points to an image file
     if path.is_file() and FileUtils().mimetype_is_image(path.app):
-
-      # get the metadata for the image
-      md = MetaData(path.app)
-
-      params = {
-        'uri': str(path.gallery),
-        'name': path.name,
-        'size': md.getImageSize(),
-        'title': md.getTitle(),
-        'description': md.getDescription(),
-        'last_modified': FileUtils().get_last_modified_datetime(path.app),
-        'parent_uri': str(path.parent)
-      }
-
-      neo4j_session.write_transaction(
-        GraphMethods().add_image,
-        **params
-      )
+      write_single_image_to_neo4j(path, neo4j_session)
 
     # check if path points to a directory
     elif path.is_dir():
+      # write the path level folder to neo4j
+      write_single_folder_to_neo4j(path, neo4j_session)
 
-      params = {
-        'uri': str(path.gallery),
-        'name': path.name,
-        'parent_uri': str(path.parent)
-      }
+      # collect all the existing resources in folder as a set
+      existing_resources = collect_existing_resources(path, neo4j_session)
 
-      neo4j_session.write_transaction(
-        GraphMethods().add_folder,
-        **params
-      )
+      # loop over subfolders and images within folder and write
+      # information to neo4j; also return parsed resources as a set
+      parsed_resources = write_folder_contents_to_neo4j(path, neo4j_session)
 
-      parse_dir(path)
+      # perform set difference and delete irrelevant nodes
+      exclude_unneeded_resources(existing_resources, parsed_resources)
 
     return "done"
 
+def write_single_image_to_neo4j(path, neo4j_session):
+  """
+  Write a single image to neo4j
+
+  Parameters:
+    path: GalleryPath
+    neo4j_session: neo4j connection
+  """
+
+  # get the metadata for the image
+  md = MetaData(path.app)
+
+  params = build_image_params(path, md)
+
+  neo4j_session.write_transaction(
+    GraphMethods().add_image,
+    **params
+  )
+
+def build_image_params(path, md):
+  """
+  Provide neo4j method parameters for creating an image entry
+
+  Parameters:
+    path: GalleryPath
+    md: instance of MetaData class
+
+  Returns:
+    dict: neo4j params
+  """
+
+  return {
+    'uri': str(path.gallery),
+    'name': path.name,
+    'size': md.getImageSize(),
+    'title': md.getTitle(),
+    'description': md.getDescription(),
+    'last_modified': FileUtils().get_last_modified_datetime(path.app),
+    'parent_uri': str(path.parent)
+  }
+
+def write_single_folder_to_neo4j(path, neo4j_session):
+  """
+  Write a single folder to neo4j
+
+  Parameters:
+    path: GalleryPath
+    neo4j_session: neo4j connection
+  """
+
+  params = build_folder_params(path)
+
+  neo4j_session.write_transaction(
+    GraphMethods().add_folder,
+    **params
+  )
+
+def build_folder_params(path):
+  """
+  Provide neo4j method parameters for creating a folder entry
+
+  Parameters:
+    path: GalleryPath
+
+  Returns:
+    dict: neo4j params
+  """
+
+  return {
+    'uri': str(path.gallery),
+    'name': path.name,
+    'parent_uri': str(path.parent)
+  }
+
 def collect_existing_resources(path, neo4j_session):
-  # collect current list of child uris
+  """
+  Collect a set of resources already existing in the
+  neo4j database. This will be used to compare against parsed
+  set to determine which nodes can be deleted
+
+  Parameters:
+    path: GalleryPath
+    neo4j_session: neo4j connection
+
+  Returns:
+    set: child resource gallery uris
+  """
 
   resources = set()
 
@@ -104,6 +164,15 @@ def collect_existing_resources(path, neo4j_session):
   return resources
 
 def exclude_unneeded_resources(existing_resources, parsed_resources):
+  """
+  Iterate through the contents of the folder and add them to the
+  neo4j database. Also, return a set of all resources added so that
+  resources not parsed may be excluded from neo4j
+
+  Parameters:
+    existing_resources: set of gallery uris
+    parsed_resources: set of gallery uris
+  """
   items_to_delete = existing_resources - parsed_resources
 
   print(items_to_delete)
@@ -118,58 +187,44 @@ def exclude_unneeded_resources(existing_resources, parsed_resources):
       **params
     )
 
-def parse_dir(path):
+def write_folder_contents_to_neo4j(path, neo4j_session):
+  """
+  Iterate through the contents of the folder and add them to the
+  neo4j database. Also, return a set of all resources added so that
+  resources not parsed may be excluded from neo4j
 
-  driver = GraphMethods.connect_to_neo4j()
+  Parameters:
+    path: GalleryPath
+    neo4j_session: neo4j connection
 
-  with driver.session() as neo4j_session:
+  Returns:
+    set: child resource gallery uris
+  """
 
-    existing_resources = collect_existing_resources(path, neo4j_session)
-    parsed_resources = set()
+  # empty set which will be populated with uris of
+  # resources in folder
+  contents_set = set()
 
-    # scan the directory specified in the path for all photos
-    # and folders, send this info to browser via websockets
-    for child in os.scandir(path.app):
+  # scan the directory specified in the path for all photos
+  # and folders, send this info to browser via websockets
+  for child in scandir(path.app):
 
-      cpath = GalleryPath(
-        PurePath(child.path).relative_to(settings.GALLERY_BASE_DIR)
-      )
+    child_path = GalleryPath(
+      PurePath(child.path).relative_to(settings.GALLERY_BASE_DIR)
+    )
 
-      if not cpath.name.startswith('.') and cpath.is_dir():
+    if not child_path.name.startswith('.') and child_path.is_dir():
+      # write the subfolder to neo4j
+      write_single_folder_to_neo4j(child_path, neo4j_session)
+      # add uri to set
+      contents_set.add(str(child_path.gallery))
 
-        params = {
-          'uri': str(cpath.gallery),
-          'name': cpath.name,
-          'parent_uri': str(cpath.parent)
-        }
+    if not child_path.name.startswith('.') and child_path.is_file() and FileUtils().mimetype_is_image(child_path.app):
+      # write the image to neo4j
+      write_single_image_to_neo4j(child_path, neo4j_session)
+      # if image thumbnails are missing, create them
+      generate_thumbnails_if_missing(child_path)
+      # add uri to set
+      contents_set.add(str(child_path.gallery))
 
-        neo4j_session.write_transaction(
-          GraphMethods().add_folder,
-          **params
-        )
-
-        parsed_resources.add(str(cpath.gallery))
-
-      if not cpath.name.startswith('.') and cpath.is_file() and FileUtils().mimetype_is_image(cpath.app):
-
-        md = MetaData(cpath.app)
-        generate_thumbnails_if_missing(cpath)
-
-        params = {
-          'uri': str(cpath.gallery),
-          'name': cpath.name,
-          'size': md.getImageSize(),
-          'title': md.getTitle(),
-          'description': md.getDescription(),
-          'last_modified': FileUtils().get_last_modified_datetime(cpath.app),
-          'parent_uri': str(cpath.parent)
-        }
-
-        neo4j_session.write_transaction(
-          GraphMethods().add_image,
-          **params
-        )
-        
-        parsed_resources.add(str(cpath.gallery))
-        
-    exclude_unneeded_resources(existing_resources, parsed_resources)
+  return contents_set
